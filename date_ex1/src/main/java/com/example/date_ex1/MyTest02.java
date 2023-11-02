670000f9cd671c4f29065865e948f46bcd704ad4
kidchang93 <lck930721@gmail.com>
2023-11-02 11:24:34 +0900

    Test02

---
+++ date_ex1/src/main/java/com/example/date_ex1/MyTest02.java
@@ -0,0 +1,17 @@
+package com.example.date_ex1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class MyTest02 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
