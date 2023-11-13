074f534e17df2ebdd76a97db146eb4ecf4d9d3d1
kidchang93 <lck930721@gmail.com>
2023-11-13 11:37:54 +0900

    JSP 이전 페이지로 가기 BOM/DOM 원리

---
+++ musthave/src/main/java/utils/JSFunction.java
@@ -0,0 +1,34 @@
+package utils;
+
+import javax.servlet.jsp.JspWriter;
+
+public class JSFunction {
+
+  // 메시지 알림창을 띄운 후 명시한 URL로 이동합니다.
+  public static void altertLocation (String msg, String url, JspWriter out) {
+    try {
+
+     String script = "" // 삽입할 자바 스크립트 코드
+             + "<script>"
+             + "  alert('" + msg + "');"
+             + "  location.href='"+ url + "';"
+             + "</script>";
+     out.print(script); // 자바 스크립트 코드를 out 내장 객체로 출력(삽입)
+    }
+    catch (Exception e) {}
+  }
+
+  // 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
+  public static void altertBack (String msg, JspWriter out) {
+    try {
+
+      String script = "" // 삽입할 자바 스크립트 코드
+              + "<script>"
+              + "  alert('" + msg + "');"
+              + "  history.back();"
+              + "</script>";
+      out.print(script);
+    }
+    catch (Exception e) {}
+  }
+}
