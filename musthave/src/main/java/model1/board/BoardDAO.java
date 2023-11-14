a1e4e85be6d78acd1e58c78e3e3fd98467de89d4
kidchang93 <lck930721@gmail.com>
2023-11-14 13:31:31 +0900

    MVC 1 모델 게시판 제작 (DAO 설정)

--- musthave/src/main/java/model1/board/BoardDAO.java
+++ musthave/src/main/java/model1/board/BoardDAO.java
@@ -21,7 +21,17 @@ public class BoardDAO extends JDBConnect {
     }
 
     try {
-
+      stmt = con.createStatement(); // 쿼리문 생성
+      rs = stmt.executeQuery(query); // 쿼리 실행
+      rs.next();                      // 커서를 첫번째 행으로 이동
+      totalCount = rs.getInt(1); // 첫번째 컬럼 값을 가져옴
+    } catch (Exception e) {
+      System.out.println("게시물 수를 구하는 중 예외 발생");
+      e.printStackTrace();
     }
+
+    return totalCount;
   }
+
+
 }
