df7fac84c28174c3490a7478f6e5fbccda075659
kidchang93 <lck930721@gmail.com>
2023-11-15 14:03:31 +0900

    MVC 1 모델 게시판 제작[DAO에 글쓰기 메서드 추가]

--- musthave/src/main/java/model1/board/BoardDAO.java
+++ musthave/src/main/java/model1/board/BoardDAO.java
@@ -75,5 +75,26 @@ public class BoardDAO extends JDBConnect {
   // 게시글 데이터를 받아 DB에 추가합니다.
   public int insertWrite(BoardDTO dto) {
     int result = 0;
+
+    try {
+      // INSERT 쿼리문 작성
+
+      String query = "INSERT INTO board ( "
+              + " title,content,id,visitcount) "
+              + " VALUES ( "
+              + " ?, ?, ?, 0)";
+
+      psmt = con.prepareStatement(query);
+      psmt.setString(1, dto.getTitle());
+      psmt.setString(2, dto.getContent());
+      psmt.setString(3, dto.getId());
+
+      result = psmt.executeUpdate();
+    }
+    catch (Exception e) {
+      System.out.println("게시물 입력 중 예외 발생");
+      e.printStackTrace();
+    }
+    return result;
   }
 }
