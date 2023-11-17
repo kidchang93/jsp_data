a98fee86dffea447535c3c629f4437faee06d174
kidchang93 <lck930721@gmail.com>
2023-11-17 13:12:15 +0900

    mvcboard SQL 문 작성

--- musthave/src/main/webapp/14MVCBoard/mvcboard.sql
+++ musthave/src/main/webapp/14MVCBoard/mvcboard.sql
@@ -1,18 +1,41 @@
+CREATE DATABASE gonsta;
+USE gonsta;
+
+
+CREATE TABLE board
+(
+    board_id    INT              NOT NULL    AUTO_INCREMENT COMMENT '게시글ID',
+    writer      VARCHAR(50)      NOT NULL    COMMENT '작성자',
+    title       VARCHAR(50)      NOT NULL    COMMENT '제목',
+    content     VARCHAR(1000)    NOT NULL    COMMENT '내용',
+    regdate     DATETIME         NOT NULL    COMMENT '등록일자',
+    updatedate  DATETIME         NULL        COMMENT '수정일자',
+    deletedate  DATETIME         NULL        COMMENT '삭제일자',
+    PRIMARY KEY (board_id)
+);
+
+
+CREATE TABLE likes(
+    member_idx	INT				 NOT NULL	 COMMENT '회원IDX'
+        post_idx	INT				 NOT NULL	 COMMENT '게시글IDX'
+        CONSTRAINT	likes_PK PRIMARY KEY (MEMBER_idx, post_idx)
+);
+
 use model1;
 select * from mvcboard;
 
 drop table if EXISTS mvcboard;
 create table mvcboard (
-idx 		int 	  primary key auto_increment,	
-name 		varchar(50) 				not null,
-title 		varchar(200) 				not null,
-content 	varchar(2000) 				not null,
-postdate 	date default (current_date) not null,
-ofile		varchar (200),
-sfile 		varchar (30),
-downcount 	int (5) 	default 0 		not null,
-pass 		varchar(50) 				not null,
-visitcount  int 		default 0 		not null
+                          idx 		int 	  primary key auto_increment,
+                          name 		varchar(50) 				not null,
+                          title 		varchar(200) 				not null,
+                          content 	varchar(2000) 				not null,
+                          postdate 	date default (current_date) not null,
+                          ofile		varchar (200),
+                          sfile 		varchar (30),
+                          downcount 	int (5) 	default 0 		not null,
+                          pass 		varchar(50) 				not null,
+                          visitcount  int 		default 0 		not null
 );
 
 
