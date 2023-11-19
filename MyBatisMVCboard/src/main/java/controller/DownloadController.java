33b916daca4ca983c9a9e9bb093358dd8d3090cc
kidchang93 <lck930721@gmail.com>
2023-11-19 18:47:44 +0900

    해내고 만다 JSP / MyBatis

---
+++ MyBatisMVCboard/src/main/java/controller/DownloadController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import Util.FileUtil;
+import model.BoardDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("")
+public class DownloadController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String ofile = req.getParameter("ofile");
+        String sfile = req.getParameter("sfile");
+        String idx = req.getParameter("idx");
+
+        FileUtil.download(req, resp, "/Uploads", sfile, ofile);
+        BoardDAO dao = new BoardDAO();
+        dao.downCoundPlus(idx);
+    }
+}
