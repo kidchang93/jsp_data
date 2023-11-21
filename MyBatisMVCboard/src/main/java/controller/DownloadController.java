40d14a2d6a7800c19aa2c6861aaa4197a2f5fd22
kidchang93 <lck930721@gmail.com>
2023-11-21 22:58:56 +0900

    MVC 2 Model 게시판 만들기 [DownloadController 주석 完]

--- MyBatisMVCboard/src/main/java/controller/DownloadController.java
+++ MyBatisMVCboard/src/main/java/controller/DownloadController.java
@@ -15,12 +15,17 @@ public class DownloadController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // 요청 파라미터에서 파일명, 저장된 파일명, 게시물 인덱스 추출
         String ofile = req.getParameter("ofile");
         String sfile = req.getParameter("sfile");
         String idx = req.getParameter("idx");
 
+        // 파일 다운로드를 위해 FileUtil의 download 메서드 호출
         FileUtil.download(req, resp, "/Uploads", sfile, ofile);
+        // 게시판 DAO 객체 생성
         BoardDAO dao = new BoardDAO();
+        // 다운로드 횟수 증가 메서드 호출
         dao.downCountPlus(idx);
     }
 }
